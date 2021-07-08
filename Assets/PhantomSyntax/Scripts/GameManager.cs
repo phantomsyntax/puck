8a28b9e6ce5858a329a0f481579149b7ca2c93b8
bronsoncox <bronsoncox@hotmail.com>
2021-07-08 13:57:47 -0500

    Add Quit
    added quit button

--- Assets/PhantomSyntax/Scripts/GameManager.cs
+++ Assets/PhantomSyntax/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +9,21 @@ namespace PhantomSyntax.Scripts {
             print("[GameManager] - HandleResetScene called"); // for debug, can be removed
             Scene activeScene = SceneManager.GetActiveScene();
             SceneManager.LoadScene(activeScene.name);
+        }
+
+        public void HandleApplicationQuit(){
+            print("[GameManager] - HandleApplicationQuit called");
+            StartCoroutine("ExitGameDelayed");
+        }
+
+        // Application Quit routine
+        private IEnumerator ExitGameDelayed() {
+            yield return new WaitForSeconds(1.0f);
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
         }
     }
 }
